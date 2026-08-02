c1a41ebff93c5f8e8760e96281e7f2495edef0d0
vennilator-sys <vennilator@gmail.com>
2026-08-02 06:16:50 -0400

    Update jobs.js

--- data/jobs.js
+++ data/jobs.js
@@ -1,1 +1,25 @@
+const occupations = [
 
+"Teacher",
+"Accountant",
+"Doctor",
+"Engineer",
+"Actor",
+"Lawyer",
+"Business Owner",
+"Musician",
+"Nurse",
+"Software Developer",
+"Writer",
+"Artist"
+
+];
+
+
+function randomOccupation(){
+
+return occupations[
+Math.floor(Math.random()*occupations.length)
+];
+
+}
