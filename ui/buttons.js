6f649a5d47b70e9771bf9ff03e20a7b7ead7d594
vennilator-sys <vennilator@gmail.com>
2026-08-02 10:54:13 -0400

    Update buttons.js

--- ui/buttons.js
+++ ui/buttons.js
@@ -1,1 +1,17 @@
+function chooseActivity(){
 
+let choice =
+prompt(
+"Choose activity:\n\nDrama Club\nSports\nAcademic Club\nGaming"
+);
+
+
+if(choice){
+
+doActivity(player, choice);
+
+Renderer.render();
+
+}
+
+}
