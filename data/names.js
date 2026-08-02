535f5c4e17761fa564cafc4ce581b6015098a567
vennilator-sys <vennilator@gmail.com>
2026-08-02 06:16:15 -0400

    Update names.js

--- data/names.js
+++ data/names.js
@@ -1,1 +1,58 @@
+const firstNames = [
 
+"James",
+"Michael",
+"Sarah",
+"Emma",
+"Olivia",
+"Ethan",
+"Noah",
+"Sophia",
+"Jackson",
+"Ava",
+"Daniel",
+"William",
+"Charlotte",
+"Benjamin"
+
+];
+
+
+const lastNames = [
+
+"Williams",
+"Carter",
+"Johnson",
+"Mitchell",
+"Brown",
+"Taylor",
+"Anderson",
+"Wilson",
+"Thomas"
+
+];
+
+
+function randomFirstName(){
+
+return firstNames[
+Math.floor(Math.random()*firstNames.length)
+];
+
+}
+
+
+function randomLastName(){
+
+return lastNames[
+Math.floor(Math.random()*lastNames.length)
+];
+
+}
+
+
+function generateName(){
+
+return `${randomFirstName()} ${randomLastName()}`;
+
+}
