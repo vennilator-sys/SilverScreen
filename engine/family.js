d92274236b7f8c0a776e44a38ed8d7a6a7fb468b
vennilator-sys <vennilator@gmail.com>
2026-08-02 06:17:22 -0400

    Update family.js

--- engine/family.js
+++ engine/family.js
@@ -1,1 +1,94 @@
 
+function createParent(name){
+
+return {
+
+name:name,
+
+age:
+Math.floor(Math.random()*15)+28,
+
+job:
+randomOccupation(),
+
+relationship:
+Math.floor(Math.random()*31)+70
+
+};
+
+}
+
+
+
+function createFamily(playerName){
+
+
+let last =
+playerName.split(" ")[1];
+
+
+return {
+
+
+father:
+createParent(
+generateName()
+),
+
+
+mother:
+createParent(
+generateName()
+),
+
+
+siblings:
+generateSiblings(last),
+
+
+wealth:
+Math.floor(Math.random()*90000)+20000
+
+
+};
+
+
+}
+
+
+
+
+function generateSiblings(last){
+
+
+let amount =
+Math.floor(Math.random()*4);
+
+
+let siblings=[];
+
+
+for(let i=0;i<amount;i++){
+
+
+siblings.push({
+
+name:
+`${randomFirstName()} ${last}`,
+
+age:
+Math.floor(Math.random()*8),
+
+relationship:
+Math.floor(Math.random()*40)+60
+
+});
+
+
+}
+
+
+return siblings;
+
+
+}
