4ae2b96395d1fc7cbe860ce3d6fb54dae0b6c13d
vennilator-sys <vennilator@gmail.com>
2026-08-02 10:41:31 -0400

    Update gameManager.js

--- game/gameManager.js
+++ game/gameManager.js
@@ -1,1 +1,19 @@
+const GameManager = {
 
+    initialize() {
+
+        // Create player
+        GameState.player = new Person();
+
+        // Compatibility alias (temporary)
+        window.player = GameState.player;
+
+        // Generate family
+        GameState.player.family =
+            createFamily(GameState.player.name);
+
+        // Draw first frame
+        Renderer.render();
+    }
+
+};
