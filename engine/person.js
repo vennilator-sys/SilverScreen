94e4fd8456af744f219ce5d47ba8a27c3ee03356
vennilator-sys <vennilator@gmail.com>
2026-08-02 10:12:03 -0400

    Update person.js

--- engine/person.js
+++ engine/person.js
@@ -1,1 +1,29 @@
+class Person {
 
+    constructor(){
+
+        this.name = generateName();
+
+        this.age = 0;
+
+        this.looks = randomStat();
+
+        this.intelligence = randomStat();
+
+        this.charisma = randomStat();
+
+        this.creativity = randomStat();
+
+        this.discipline = randomStat();
+
+        this.actingTalent = randomStat();
+
+        this.directingTalent = randomStat();
+
+        this.writingTalent = randomStat();
+
+        this.networking = randomStat();
+
+    }
+
+}
