4f07d929ddfbe0db37211af67cec1c441890941d
vennilator-sys <vennilator@gmail.com>
2026-08-03 18:16:01 -0400

    Update school.js

--- engine/school.js
+++ engine/school.js
@@ -1,1 +1,24 @@
+const School = {
 
+    advanceYear(player) {
+
+        const result = {
+
+            history: []
+
+        };
+
+        if(player.age < 5)
+            return result;
+
+        // grade progression
+
+        // GPA
+
+        // reputation
+
+        return result;
+
+    }
+
+};
