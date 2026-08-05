c2cdaef17ed02d157d916f3b728ff3e071dc4ad5
vennilator-sys <vennilator@gmail.com>
2026-08-04 23:53:28 -0400

    Update relationshipEngine.js

--- engine/relationshipEngine.js
+++ engine/relationshipEngine.js
@@ -1,1 +1,30 @@
+const RelationshipManager = {
 
+
+    createFriend(player) {
+
+
+        const friend = new Person();
+
+
+        friend.age = player.age;
+
+
+        player.relationships.friends.push(friend);
+
+
+        return {
+
+            history: [
+
+                `${friend.name} became your friend.`
+
+            ]
+
+        };
+
+
+    }
+
+
+};
